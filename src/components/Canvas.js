29925f3973fca16ed1572d42645692c8440e4292
Pedro Ferrari <pedro@pedroferrari.com>
2018-08-23 00:46:20 +0200

    Create React App Setup

---
+++ src/components/Canvas.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Canvas extends Component {
+  render() {
+    return (
+      <canvas id="tetris" width="240" height="400"></canvas>
+    )
+  }
+}
