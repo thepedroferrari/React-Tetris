f09a6b4f051f51e95130e62d4ac162c037f3e637
Pedro Ferrari <pedro@pedroferrari.com>
2018-08-23 02:14:14 +0200

    first Matrix Draw

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
 
+const rect = (props) => {
+	const { ctx, x, y, width, height, color } = props;
+	ctx.fillStyle = color;
+	ctx.fillRect(x, y, width, height);
+};
+
+// prettier-ignore
+const matrix = [
+	[ 0, 0, 0 ],
+  [ 1, 1, 1 ],
+  [ 0, 1, 0 ]
+];
+
 export default class Canvas extends Component {
 	componentDidMount() {
+		this.updateCanvas();
+	}
+	componentDidUpdate() {
 		this.updateCanvas();
 	}
 	updateCanvas() {
 		const canvas = this.refs.canvas;
 		const ctx = canvas.getContext('2d');
-		ctx.fillStyle = '#f00';
-		ctx.fillRect(0, 0, 100, 100);
+		ctx.scale(20, 20);
+
+		// Draw Canvas Background
+		rect({ ctx, x: 0, y: 0, width: canvas.width, height: canvas.height });
+		matrix.forEach((row, y) => {
+			row.forEach((value, x) => {
+				console.log(value);
+				if (value) {
+					rect({ ctx, x, y, width: 1, height: 1, color: '#f00' });
+				}
+			});
+		});
 	}
 	render() {
-		return <canvas id="tetris" ref="canvas" width={240} height={400} />;
+		return <canvas id="tetris" ref="canvas" width={360} height={600} />;
 	}
 }
